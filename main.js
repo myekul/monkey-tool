9593a0d0e8bb26a93a56106c297b21747f9a7ea7
myekul <somebodynamedmichael@gmail.com>
2025-07-24 02:24:03 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,47 @@
+setTitle('MONKEY TOOL')
+setFooter('2025')
+const map = {
+    1: 'mdhrCrest',
+    2: 'mdhrOther',
+    3: 'cuphead',
+    4: 'mugman',
+    5: 'diceLeft',
+    6: 'diceRight',
+}
+const patterns = {
+    16: [[1, 2, 4, 6], [6, 1, 3, 3], [5, 4, 2, 5]],
+    17: [[1, 6, 3, 3], [4, 2, 1, 5], [5, 4, 6, 2]],
+    25: [[4, 1, 5, 3], [1, 2, 5, 6], [3, 6, 2, 4]],
+    210: [[3, 1, 4, 2], [4, 3, 5, 6], [6, 1, 2, 5]],
+    39: [[4, 5, 1, 5], [6, 2, 3, 4], [1, 6, 3, 2]],
+    312: [[3, 4, 1, 4], [5, 1, 5, 6], [3, 6, 2, 1]],
+    49: [[2, 6, 3, 1], [3, 4, 5, 6], [1, 5, 4, 2]],
+    512: [[6, 4, 3, 3], [1, 2, 4, 5], [5, 2, 6, 1]],
+    69: [[2, 6, 2, 3], [3, 1, 4, 6], [1, 4, 5, 5]],
+    611: [[5, 2, 2, 4], [6, 1, 5, 3], [4, 3, 1, 6]],
+    79: [[5, 3, 4, 5], [2, 6, 1, 3], [1, 6, 2, 4]],
+    811: [[6, 3, 5, 4], [5, 2, 6, 1], [3, 4, 1, 2]]
+}
+function createTable(code) {
+    let HTMLContent = ''
+    const pattern = patterns[code]
+    if (pattern) {
+        HTMLContent += `<table>`
+        pattern.forEach(row => {
+            HTMLContent += `<tr>`
+            row.forEach(cell => {
+                HTMLContent += `<td class='${map[cell]}'></td>`
+            })
+        })
+        HTMLContent += `</table>`
+    } else {
+        HTMLContent += `
+        <table>
+        <tr><td></td><td></td><td></td><td></td></tr>
+        <tr><td></td><td></td><td></td><td></td></tr>
+        <tr><td></td><td></td><td></td><td></td></tr>
+        </table>`
+    }
+    document.getElementById('tableDiv').innerHTML = HTMLContent
+}
+createTable()
